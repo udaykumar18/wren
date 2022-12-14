e25d83127c7f3eb383171adf4cfd2ea520e307c3
udaykumar18 <114995146+udaykumar18@users.noreply.github.com>
2022-12-14 18:33:22 +0530

    website v2
    v2 - hero section

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const hamburgerMenu = document.getElementById("toggle");
-
+const sliderBar = document.getElementById("menu");
 hamburgerMenu.onclick = function () {
   hamburgerMenu.classList.toggle("active");
+  sliderBar.classList.toggle("active");
 };
