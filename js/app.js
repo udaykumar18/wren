99f65996f835c34249d77c6e25fff17e8e40ca59
udaykumar18 <114995146+udaykumar18@users.noreply.github.com>
2022-12-19 22:07:58 +0530

    website v4
    v4-updated sections

--- js/app.js
+++ js/app.js
@@ -4,3 +4,36 @@ hamburgerMenu.onclick = function () {
   hamburgerMenu.classList.toggle("active");
   sliderBar.classList.toggle("active");
 };
+
+// SWIPER JS
+
+//swiper js
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 2,
+  coverflowEffect: {
+    rotate: 30,
+    stretch: 0,
+    depth: 300,
+    modifier: 1,
+    slideShadows: true,
+  },
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
